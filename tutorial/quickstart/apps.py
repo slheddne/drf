95d816c95ccf546e6b43bd156d3783f94ad30e5a
Salah-Eddine <31400rng@gmail.com>
2024-01-07 17:51:53 +0100

    Added: Comments

--- tutorial/quickstart/apps.py
+++ tutorial/quickstart/apps.py
@@ -1,6 +1,8 @@
+# Imports -> Standard
 from django.apps import AppConfig
 
 
+# Config -> Quickstart config to represent the Quickstart app
 class QuickstartConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'quickstart'
