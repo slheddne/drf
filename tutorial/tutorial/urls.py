ccf2d7ea3e80675ee22b6ea369fc361bac027f88
Seiya <salah_ettaleby@icloud.com>
2024-01-07 09:06:43 +0100

    Added: Comments

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -4,7 +4,10 @@ from rest_framework import routers
 
 from quickstart import views
 
+# Routers -> Default router to register the viewsets
 router = routers.DefaultRouter()
+
+# Routers -> Register the viewsets
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'songs', views.SongViewSet)
@@ -18,4 +21,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
+# URLS -> Add the router urls
 urlpatterns += router.urls
